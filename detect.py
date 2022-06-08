d1014c6a8f9eb16a8b443dcea4b0a3f064bdc9a3
Stephan Wiesenhütter <stephan.wiesenhuetter@eulitha.com>
2022-06-08 22:09:14 +0200

    find convexity defects and assume 4 in a cross

--- detect.py
+++ detect.py
@@ -20,6 +20,7 @@ def cross_center(outline):
 
 
 image = cv2.imread('data/low-contrast-x.png', cv2.IMREAD_GRAYSCALE)
+cv2.imshow('original image', image)
 
 thresh = 10
 ret, thresh_img = cv2.threshold(image, thresh, 255, cv2.THRESH_BINARY)
@@ -31,10 +32,7 @@ img_contours = np.zeros(thresh_img.shape)
 # draw the contours on the empty image
 cv2.drawContours(img_contours, contours, -1, 255, 1)
 cv2.imwrite('data/contours.jpg', (255 * img_contours))
-
-
 cv2.imshow('contour mage', img_contours)
-cv2.imshow('contour mage', image)
 
 img_simpler = np.ones(thresh_img.shape)
 
@@ -44,14 +42,27 @@ for cont in contours:
     silen = cv2.arcLength(simpler, True)
 
     x_hull = simpler.reshape((len(simpler), 2))
-    
-    if len(x_hull) == 8 or len(x_hull) == 12:
-        x, y = cross_center(x_hull)
-        cv2.polylines(img_simpler, [x_hull], True, 0, 1)
-        cv2.putText(img_simpler, "x: " + str(x) + "y: " + str(y), (int(x), int(y)), cv2.FONT_HERSHEY_SIMPLEX, 0.5, 0, 1)
-        cv2.circle(img_simpler, (int(x), int(y)), 5, 0, -1)
-    else:
-        cv2.polylines(img_simpler, [x_hull], True, 0, 1)
+
+    convex_hull = cv2.convexHull(x_hull, returnPoints = True)
+    cv2.polylines(img_simpler, [convex_hull], True, 0, 1)
+    if len(x_hull) > 4:
+        convex_hull = cv2.convexHull(x_hull, returnPoints = False)
+        try:
+            convexityDefects = cv2.convexityDefects(cont, convex_hull)
+        except Exception as e:
+            print(e)
+            continue
+        x = x_hull[0][0]
+        y = x_hull[0][1]
+        cv2.putText(img_simpler, "def {}".format((len(convexityDefects))), (x, y), cv2.FONT_HERSHEY_SIMPLEX, 0.5, 0, 1)
+
+        if len(convexityDefects) == 4:
+            x, y = cross_center(x_hull)
+            cv2.polylines(img_simpler, [x_hull], True, 0, 1)
+            cv2.putText(img_simpler, "x: " + str(x) + "y: " + str(y), (int(x), int(y)), cv2.FONT_HERSHEY_SIMPLEX, 0.5, 0, 1)
+            cv2.circle(img_simpler, (int(x), int(y)), 5, 0, -1)
+        else:
+            cv2.polylines(img_simpler, [x_hull], True, 0, 1)
 
 cv2.imshow('simpler mage', img_simpler)
 
